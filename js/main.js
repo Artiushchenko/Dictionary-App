603a6c6ff81ee746b24c0764e144cdf079da7c95
Artyushchenko <Artyushchenko2018@gmail.com>
2024-07-16 15:29:52 +0300

    Theme change was realized

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@ const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 const searchBlock = document.getElementById('search-result');
 const sound = document.getElementById('word-pronouncing');
 const searchButton = document.getElementById('search-button');
+const themeToggleButton = document.getElementById('theme-toggle-button');
+const themeIcon = document.getElementById('theme-icon');
+
+/* LOAD THEME FROM COOKIES */
+
+document.addEventListener('DOMContentLoaded', () => {
+	const theme = getCookie('theme');
+
+	if (theme) {
+		document.body.classList.add(theme);
+	}
+});
+
+/* SEARCH THE WORD */
 
 searchButton.addEventListener('click', () => {
 	let inputWord = document.getElementById('input-word').value;
@@ -38,6 +52,57 @@ searchButton.addEventListener('click', () => {
 		});
 });
 
+/* PLAY PRONUNCIATION  */
+
 function playSound() {
 	sound.play();
+}
+
+/* CHANGE THEME */
+
+themeToggleButton.addEventListener('click', () => {
+	if (document.body.classList.contains('dark-theme')) {
+		document.body.classList.remove('dark-theme');
+		themeIcon.classList.remove('fa-sun');
+		themeIcon.classList.add('fa-moon');
+
+		/* REMOVE COOKIE */
+		setCookie('theme', '', -1);
+	} else {
+		document.body.classList.add('dark-theme');
+		themeIcon.classList.remove('fa-moon');
+		themeIcon.classList.add('fa-sun');
+
+		/* SET COOKIE FOR 7 DAYS */
+		setCookie('theme', 'dark-theme', 7);
+	}
+});
+
+/* HANDLE COOKIE */
+
+function setCookie(cookieName, cookieValue, days) {
+	const date = new Date();
+	date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+
+	const expires = 'expires=' + date.toUTCString();
+
+	document.cookie = cookieName + '=' + cookieValue + ';' + expires + ';path=/';
+
+	console.log(`Cookie set: ${cookieName}=${cookieValue}; ${expires}`);
+}
+
+function getCookie(cookieName) {
+	const name = cookieName + '=';
+	const decodeCookie = decodeURIComponent(document.cookie);
+	const cookieArray = decodeCookie.split(';');
+
+	for (let i = 0; i < cookieArray.length; i++) {
+		let cookie = cookieArray[i].trim();
+
+		if (cookie.indexOf(name) === 0) {
+			return cookie.substring(name.length, cookie.length);
+		}
+	}
+
+	return '';
 }
